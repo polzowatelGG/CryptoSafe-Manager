9bbb4403df5189c7e9fefbb10a44e023a1cb3089
polzowatelGG <creep123gleb@gmail.com>
2026-02-16 22:53:19 +0300

    расширил возможности StateManager. добавил сеттер, геттер и таймеры

--- src/core/config.py
+++ src/core/config.py
@@ -1,8 +1,11 @@
-
+import threading
 
 class StateManager:
     def __init__(self):
         self.session_locked = False # при создании новой сессии она будет разблокирована 
+        self.clipboard = None # содержимое буффера обмена 
+        self.clipboard_timer = None # таймер очистки буффера по умолчанию 
+        self.clipboard_timeout = 90 # хранение данных в буффере 90 сек. 
         
     def lock(self):
         self.session_locked = True 
@@ -14,5 +17,23 @@ class StateManager:
         
     def is_locked(self):
         return self.session_locked
+    
+    def set_clipboard(self, data):
+        self.clipboard = data # обновляем буффер обмена  
+        self.clipboard_timer() # вкл таймер
+        
+    def get_clipboard(self, data):
+        return self.clipboard 
+    
+    def clear_clipboard(self):
+        self.clipboard = None # очищаем буффер обмена 
+        
+    def start_clipboard_timer(self):
+        if self.clipboard_timer:
+            self.clipboard_timer.cancel() # если таймер уже запущен - отменяем 
+            
+        self.clipboard_timer = threading.Timer(self.clipboard_timeout, self.clear_clipboard)
+        self.clipboard_timer.start() # создаём новый таймер для очистки
+        
     
     #first try
