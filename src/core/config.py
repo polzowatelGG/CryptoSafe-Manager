a14e37ac1f7ef5298b621683d273a58bc59f520c
polzowatelGG <creep123gleb@gmail.com>
2026-02-17 20:32:03 +0300

    добавлены сеттер,геттер,сохранение настроек

--- src/core/config.py
+++ src/core/config.py
@@ -2,13 +2,13 @@ import json
 from pathlib import Path
 
 class ConfigManager:
-    DEFAULTS = { 
+    DEFAULTS = {                                # использую словарь для хранения дефолтных настроек (ключ - название настройки, значение - её дефолтное значение)
     "db_path": "data/cryptosafe.db",            # путь к файлу базы данных
     "encryption_algorithm": "AESPlaceholder",   # заглушка для алгоритма шифрования
     "clipboard_timeout": 30,                    
     "auto_lock_timeout": 300,
     "environment": "dev",                       # текущее окружение (dev, staging, prod)
-    }                                           # использую словарь для хранения дефолтных настроек (ключ - название настройки, значение - её дефолтное значение)
+    }                                           
     
     def __init__(self, env: str = "dev"):       # при инициализации класса указываем окружение (по умолчанию - dev)
         self.env = env                          # сохранение среды внутри объекта для дальнейшего использования
@@ -16,7 +16,17 @@ class ConfigManager:
         self.settings = self.DEFAULTS.copy()    # создаём копию дефолтных настроек, чтобы можно было изменять их без изменения оригинала
         self.load()                             # при создании объекта сразу загружаем настройки из файла (если он существует) или используем дефолтные значения
         
-    def load(self):                             # 
+    def load(self):                             
         if self.config_path.exists():           # проверяем, существует ли файл конфигурации
             with open(self.config_path, "r") as f:  # если файл есть, открываем его для чтения
                 self.settings.update(json.load(f))  # загружаем настройки из файла и обновляем текущие настройки (если в файле есть какие-то значения, они перезапишут дефолтные)
+                
+    def save(self):
+        with open(self.config_path, "w") as f:  # открываем файл конфигурации для записи (если файла нет, он будет создан)
+            json.dump(self.settings, f, indent=4)  # сохраняем текущие настройки в файл в формате JSON с отступами для удобства чтения
+            
+    def get(self, key, default=None):
+        return self.settings.get(key, default)  # метод для получения значения настройки по ключу 
+
+    def set(self, key, value):
+        self.settings[key] = value  # метод для установки значения настройки по ключy 
