ff11d35b3d1b484f78adef45f5af5576f429cad7
polzowatelGG <creep123gleb@gmail.com>
2026-02-24 23:11:39 +0300

    прогресс 21-24.02: создан абстрактный класс для помощи с шифрованием/расшифрованием данных

---
+++ src/core/crypto/abstract.py
@@ -0,0 +1,6 @@
+class EncryptionServiceL:
+    def encrypt(self, data: bytes, key: bytes): #зашифровать данные с помощью ключа
+        pass
+
+    def decrypt(self, ciphertext: bytes, key: bytes): #расшифровать данные с помощью ключа
+        pass
